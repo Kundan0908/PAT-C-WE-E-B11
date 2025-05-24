2222b07ec3c5512752d7872c333f3de512603095
Kundan0908 <108326530+Kundan0908@users.noreply.github.com>
2025-05-24 17:13:21 +0530

    Add files via upload

---
+++ Python_Session-2/dictionary.py
@@ -0,0 +1,8 @@
+# dictionary is a data type. -> mutable data type --> It stores item in a key value pair
+#it is enclosed by {'key':'value'} and comma seprated.
+
+dict_ = {'name':'kundan','role':'tutor'}
+print(type(dict_))
+
+dict_['experience'] = 6
+print(dict_)
